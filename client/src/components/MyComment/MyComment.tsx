c421969797b042e78ab16daaf9770703fdb096f8
choigicheol <kofpo900916@gmail.com>
2021-10-21 20:56:14 +0900

    Client initial setting

---
+++ client/src/components/MyComment/MyComment.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyComment() {
+  return <div></div>;
+}
+
+export default MyComment;
---
+++ client/src/components/MyPost/MyPost.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyPost() {
+  return <div></div>;
+}
+
+export default MyPost;
