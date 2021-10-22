7b34e8be9e4fa425e3971a7eb161d170685fba1d
devSominPark <min85472@gmail.com>
2021-10-22 11:08:18 +0900

    Client initial setting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,29 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import CreatePostPage from "./pages/MainPage/CreatePostPage/CreatePostPage";
+import DetailPage from "./pages/MainPage/DetailPage/DetailPage";
+import EditPostPage from "./pages/MainPage/EditPostPage/EditPostPage";
+import MainPage from "./pages/MainPage/MainPage/MainPage";
+import EditProfilePage from "./pages/MyPage/EditProfilePage/EditProfilePage";
+import MyPage from "./pages/MyPage/MyPage/MyPage";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={LandingPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/signup" component={SignUpPage} />
+        <Route path="/main" component={MainPage} />
+        <Route path="/detail" component={DetailPage} />
+        <Route path="/createpost" component={CreatePostPage} />
+        <Route path="/editpost" component={EditPostPage} />
+        <Route path="/profile" component={MyPage} />
+        <Route path="/editprofile" component={EditProfilePage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
