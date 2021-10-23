3d40b22785df3c88c4cc72388070c97491b5e9fc
devSominPark <min85472@gmail.com>
2021-10-23 16:53:01 +0900

    [LoginPage][Login] 로그인 페이지 레이아웃

--- client/src/pages/LoginPage/LoginPage.tsx
+++ client/src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import {
+  Container,
+  ContentContainer,
+  TitleWrapper,
+  Title,
+  InputTitle,
+  InputField,
+  InvalidMessage,
+  LoginBtn,
+  SocialLoginBtn,
+  SignupBtn,
+} from "./LoginPage.style";
 
 function LoginPage() {
-  return <div></div>;
+  return (
+    <Container>
+      <ContentContainer>
+        <TitleWrapper>
+          <Title>로그인</Title>
+        </TitleWrapper>
+        <InputTitle>이메일</InputTitle>
+        <InputField />
+        <InputTitle>비밀번호</InputTitle>
+        <InputField type="password" />
+        <InvalidMessage>
+          <img src="./images/warning.svg" alt="warning" />
+          <div>이메일과 비밀번호를 다시한번 확인해 주세요.</div>
+        </InvalidMessage>
+        <LoginBtn>로그인</LoginBtn>
+        <SocialLoginBtn>
+          <img src="./images/googleLogo.svg" alt="google" />
+          <div>구글 로그인</div>
+        </SocialLoginBtn>
+        <SocialLoginBtn>
+          <img src="./images/kakaoLogo.svg" alt="kakao" />
+          <div>카카오 로그인</div>
+        </SocialLoginBtn>
+        <SignupBtn>아직 이메일이 없으신가요? 회원가입 하러가기</SignupBtn>
+      </ContentContainer>
+    </Container>
+  );
 }
 
 export default LoginPage;
