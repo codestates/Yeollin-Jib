f89473aac0e620bc04a505f2123ea89264265015
devSominPark <min85472@gmail.com>
2021-10-27 18:14:38 +0900

    [MyPage][Card] 데이터가 없을 때 나타나는 카드 컴포넌트

--- client/src/components/MyChattingRoom/MyChattingRoom.tsx
+++ client/src/components/MyChattingRoom/MyChattingRoom.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { CardContainer } from "./MyChattingRoom.style";
 
 function MyChattingRoom() {
-  return <div></div>;
+  return (
+    <>
+      <CardContainer>
+        <div>진행중인 채팅이 없습니다.</div>
+      </CardContainer>
+    </>
+  );
 }
 
 export default MyChattingRoom;
