c6a69d48f213bb0d81fc5464b556028c19a93af2
Jung InYong <83904581+iysh321@users.noreply.github.com>
2021-10-29 08:01:13 +0900

    [배포][자동화] 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/im-sprint-practice-deploy/server
+cd /home/ubuntu/Yeollin-Jib/server
 pm2 stop ./dist/index.js 2> /dev/null || true
 pm2 delete ./dist/index.js 2> /dev/null || true
