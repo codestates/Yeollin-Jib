15504903cfce5a8e65476baf8ccaf2f5126a0885
Jung InYong <iysh321@gmail.com>
2021-10-29 06:41:57 +0900

    [배포][서버] scripts 수정

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,15 +5,7 @@ import app from "./app";
 const PORT: number = parseInt(process.env.PORT as string, 10) || 4000;
 const HOST: string = process.env.HOST || "localhost";
 
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
   console.log(`Server Listening on ${PORT}`);
   //sequelize-db 연결 테스트
-  await sequelize
-    .authenticate()
-    .then(async () => {
-      console.log("connection success");
-    })
-    .catch((e) => {
-      console.log("TT : ", e);
-    });
 });
