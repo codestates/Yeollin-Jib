d9aff341721f13ad2505c53fbe513ce8de59a420
Suzy Hwang <soyeongsuzyhwang@gmail.com>
2021-11-01 19:00:58 +0900

    [LandingPage][Layout] 랜딩페이지 레이아웃 완료

--- client/src/pages/LandingPage/LandingPage.tsx
+++ client/src/pages/LandingPage/LandingPage.tsx
@@ -10,6 +10,7 @@ import {
   FirstBodyContainer,
   BigCircle,
   SmallCircle,
+  VerySmallCircle,
   SecLandingContainer,
   SecBodyContainer,
   SecBoxContainer,
@@ -17,6 +18,15 @@ import {
   SecBigTxtContainer,
   SecDescrContainer,
   SecAllBoxContainer,
+  ThirLandingContainer,
+  ThirBodyContainer,
+  ThirTextContainer,
+  SevLandingContainer,
+  ThirImageContainer,
+  TitleContainer,
+  DescrContainer,
+  AllContainer,
+  Circle,
 } from "./LandingPage.style";
 
 function LandingPage() {
@@ -41,9 +51,48 @@ function LandingPage() {
     },
   ];
 
+  const LandingPageTxt = [
+    {
+      title: ["01.", "우리 지역을 검색해", "어떤 열린집들이 있는지 살펴보세요"],
+      img: "./images/landing2.svg",
+      descr: ["지역별, 물품별로 보다 쉽게 검색이 가능합니다."],
+    },
+    {
+      title: [
+        "02.",
+        "게시글 내 위치를 확인하고 채팅을 보내",
+        "열린집 방문 약속을 잡으세요",
+      ],
+      img: "./images/landing3.svg",
+      descr: [
+        "만남 위치가 지도로 삽입되어 있어 확인이",
+        "편리하고, 글 작성자와 1:1 채팅을 할 수 있습니다.",
+      ],
+    },
+    {
+      title: ["03.", "열린집에 방문해서", "즐거운 나눔을 진행하세요"],
+      img: "./images/landing4.svg",
+      descr: ["열린집에서 마음에 드는 물품을 나눔받으세요."],
+    },
+    {
+      title: ["04.", "마감시간이 지난 게시글은", "두고 떠나세요"],
+      img: "./images/landing5.svg",
+      descr: [
+        "지정한 나눔시간이 지나면",
+        "열린집에서 자동으로 나눔완료 처리됩니다.",
+      ],
+    },
+  ];
+
+  // 마지막 버튼 밑으로 스크롤이 내려간 상태로 메인으로 연결되는 것을 방지해 주는 핸들러
+  const scrollHandler = () => {
+    window.scrollTo({ top: 0, left: 0 });
+  };
+
   return (
     <AllLandingContainer>
       <SmallCircle></SmallCircle>
+      <VerySmallCircle></VerySmallCircle>
       <FirstLandingContainer>
         <BigCircle></BigCircle>
         <FirstBodyContainer>
@@ -87,6 +136,41 @@ function LandingPage() {
           </SecAllBoxContainer>
         </SecBodyContainer>
       </SecLandingContainer>
+      {LandingPageTxt.map((el, idx) => {
+        return (
+          <ThirLandingContainer>
+            <ThirBodyContainer>
+              <AllContainer idx={idx}>
+                <ThirTextContainer>
+                  <Circle></Circle>
+                  <TitleContainer>
+                    {el.title.map((el) => {
+                      return <div>{el}</div>;
+                    })}
+                  </TitleContainer>
+                  <DescrContainer>
+                    {el.descr.map((el) => {
+                      return <div>{el}</div>;
+                    })}
+                  </DescrContainer>
+                </ThirTextContainer>
+                <ThirImageContainer idx={idx}>
+                  {" "}
+                  <img src={el.img} alt={el.title[0]}></img>
+                </ThirImageContainer>
+              </AllContainer>
+            </ThirBodyContainer>
+          </ThirLandingContainer>
+        );
+      })}
+      <SevLandingContainer>
+        {" "}
+        <Link to="/main">
+          <GotoMainButton onClick={() => scrollHandler()}>
+            열린집 보러가기
+          </GotoMainButton>
+        </Link>
+      </SevLandingContainer>
     </AllLandingContainer>
   );
 }
