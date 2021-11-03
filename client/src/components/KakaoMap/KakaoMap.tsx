75a8f67006be77741fc8e27d5dae2c9faab2b426
devSominPark <min85472@gmail.com>
2021-11-03 22:19:04 +0900

    [MyPage][EditProfile] 지도 api css 변경 및 kakao map과의 검색 연결

--- client/src/components/KakaoMap/KakaoMap.tsx
+++ client/src/components/KakaoMap/KakaoMap.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
-export const KakaoMapViewer = styled.div`
+const KakaoMapViewer = styled.div`
   box-sizing: border-box;
   width: 100%;
   height: 430px;
@@ -53,18 +53,48 @@ const KakaoMap = ({ addressInput }: AddressName) => {
           searchCenter.push(result[0].x);
           setMapCenter(searchCenter);
 
+          // 지도 우측에 확대 축소 컨트롤을 생성
+          const zoomControl = new window.kakao.maps.ZoomControl();
+          map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
+
+          const imageSrc = "images/mapMarker.png"; // 마커 이미지 주소
+          const imageSize = new window.kakao.maps.Size(60, 72); // 마커 이미지 크기
+          const imageOption = {
+            offset: new window.kakao.maps.Point(20, 69), // 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정
+          };
+
+          const markerImage = new window.window.kakao.maps.MarkerImage(
+            imageSrc,
+            imageSize,
+            imageOption
+          );
+
           // 결과값으로 받은 위치를 마커로 표시합니다
           let marker = new window.kakao.maps.Marker({
+            image: markerImage,
             map: map,
             position: coords,
           });
 
-          let infowindow = new window.kakao.maps.InfoWindow({
-            content:
-              '<div style="width:150px;text-align:center;padding:6px 0;">열린집</div>',
+          marker.setMap(map);
+
+          // 카카오맵에서 검색되도록 주소에서 공백을 지워줌
+          const searchAddress = addressInput?.replace(/ /g, "");
+
+          // customOverlay CSS
+          var content = `<a style="text-decoration: none;" href=https://map.kakao.com/link/search/${searchAddress} target="_blank">
+          <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; background-color: #fff; border: 1px solid #e0dde1; border-radius: 0.313rem; width: 100%; height: 100%; padding: 10px; margin-bottom: 200px; cursor: pointer;">
+          <span style="font-family: Gmarket Sans TTF; color: #2d2d2d; font-weight: 300; font-size: 1rem; margin-bottom: 5px;">열린집 위치</span>
+          <span style="font-family: Gmarket Sans TTF; color: #2d2d2d; font-weight: 100; font-size: 0.9rem;">${addressInput}</span>
+          </div>
+          </a>`;
+
+          var customOverlay = new window.kakao.maps.CustomOverlay({
+            position: marker.getPosition(),
+            content: content,
           });
 
-          infowindow.open(map, marker);
+          customOverlay.setMap(map);
 
           // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
           map.setCenter(coords);
