8001108dea094e0e41cd890f76adaf998f4a87c8
Suzy Hwang <soyeongsuzyhwang@gmail.com>
2021-11-09 02:55:26 +0900

    [Modal][Loading] 로딩창 CSS 수정

--- client/src/components/Loading/Loading.tsx
+++ client/src/components/Loading/Loading.tsx
@@ -4,16 +4,26 @@ import { ModalBackground, Container, Image } from "./Loading.style";
 
 const animationImages = [
   "Images/8.svg",
-  "Images/7.svg",
-  "Images/6.svg",
-  "Images/5.svg",
-  "Images/4.svg",
-  "Images/3.svg",
+  "Images/8.svg",
+  "Images/8.svg",
+  "Images/8.svg",
+  "Images/1.svg",
+  "Images/1.svg",
   "Images/2.svg",
-  "Images/1.svg",
-  "Images/1.svg",
-  "Images/1.svg",
-  "Images/1.svg",
+  "Images/3.svg",
+  "Images/4.svg",
+  "Images/5.svg",
+  "Images/6.svg",
+  "Images/7.svg",
+  "Images/8.svg",
+  "Images/8.svg",
+  "Images/8.svg",
+  "Images/8.svg",
+  "Images/8.svg",
+  "Images/8.svg",
+  "Images/8.svg",
+  "Images/8.svg",
+  "Images/8.svg",
 ];
 
 const App = () => {
@@ -23,7 +33,7 @@ const App = () => {
     let count = 0;
     let countInterval = setInterval(() => {
       setImageNumber(count++ % 10);
-    }, 1000 / 10);
+    }, 1000 / 13);
 
     return () => clearInterval(countInterval);
   }, []);
