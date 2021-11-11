47c45779eb28b279422adf81dd8981442a2bb340
Suzy Hwang <soyeongsuzyhwang@gmail.com>
2021-11-12 03:16:38 +0900

    [User][EndPoint] 유저 탈퇴 시 게시글 내 모든 찜하기, 댓글 삭제

--- server/src/controllers/user/delete.ts
+++ server/src/controllers/user/delete.ts
@@ -3,7 +3,7 @@ import comment from "../../models/comment";
 import user from "../../models/user";
 import storage from "../../models/storage";
 import post from "../../models/post";
-
+import post_category from "../../models/post_category";
 const delete_ = async (req: Request, res: Response) => {
   try {
     const header: object = req.headers;
@@ -12,10 +12,29 @@ const delete_ = async (req: Request, res: Response) => {
       return res.status(403).json({ message: "잘못된 요청입니다." });
     } else {
       const userId = req.cookies.id;
+      const findPostId: any = await post.findAll({
+        where: { userId },
+        attributes: ["id"],
+      });
+      // 내가 쓴 게시글의 id를 받아서 하나씩 다 지워주기
+      let postId;
+      if (findPostId) {
+        for (let i = 0; i < findPostId.length; i++) {
+          postId = findPostId[i].dataValues.id;
+          await comment.destroy({ where: { postId } });
+          await storage.destroy({ where: { postId } });
+          await post_category.destroy({ where: { postId } });
+        }
+      }
       await user.destroy({ where: { id: userId } });
       await comment.destroy({ where: { userId } });
       await storage.destroy({ where: { userId } });
-      await post.destroy({ where: { userId } });
+      await post.destroy({
+        where: {
+          userId,
+        },
+      });
+
       return res
         .status(200)
         .cookie("refreshToken", "")
