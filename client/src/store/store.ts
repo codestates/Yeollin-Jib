ee8538f757fafd2b321a3452b07563049273e1a7
devSominPark <min85472@gmail.com>
2021-11-12 10:46:50 +0900

    [MainPage][DetailPage] 로거 제거 및 상세페이지 로그인 필요 모달 연결

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -3,8 +3,6 @@ import { createLogger } from "redux-logger";
 import { persistStore } from "redux-persist";
 import rootReducer from "../reducers/rootReducer";
 
-const logger = createLogger();
-
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
@@ -16,7 +14,7 @@ export const store = configureStore({
           "userReducer/setUser/fulfilled",
         ],
       },
-    }).concat(logger),
+    }),
 });
 
 export const persistor = persistStore(store);
