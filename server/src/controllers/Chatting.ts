f56b33baded7f8b2724428556c4f91abd7fe66cb
Jung InYong <iysh321@gmail.com>
2022-04-04 18:33:45 +0900

    [Controller/Data] 채팅기본구현

--- server/src/controllers/Chatting.ts
+++ server/src/controllers/Chatting.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { Socket } from "../connection/socket";
 
 import { Container } from "inversify";
@@ -20,99 +20,168 @@ export class ChattingController {
     const userId: number = Number(req.cookies.id);
     const { contents } = req.body;
 
-    const tweet = await chattingRepository.createChat(userId, contents);
-    Socket.getSocketIO().emit("tweets", tweet);
-    return res.status(201).json(tweet);
+    const chat = await chattingRepository.createChat(userId, contents);
+    const chatId = chat.id;
+    const chatData = await chattingRepository.getChatById(chatId);
+
+    Socket.getSocketIO().emit("chatting", chatData);
+    return res.status(201).json(chatData);
   };
 
-  //   // 방 입장
-  //   socket.on("joinRoom", (room) => {
-  //     socket.join(room);
-  //   });
+  getChats = async (req: Request, res: Response) => {
+    const chattingRepository = this.container.get<ChattingData>(
+      TYPES.chattingDB,
+    );
+    const userId: number = Number(req.cookies.id);
 
-  //   //채팅 메시지
-  //   socket.on("sendMessage", async ({ room, name, message, chatLength }) => {
-  //     try {
-  //       const nowProject = await chatroom.findOne({
-  //         where: {},
-  //       });
-  //       const nowUser = await user.findOne({
-  //         where: {},
-  //       });
-  //       let chat = await chatting.create({
-  //         text: message,
-  //         uploadImage: "",
-  //         writer: nowUser,
-  //         project: nowProject,
-  //         room,
-  //       });
-  //       await chat.save();
-  //       socket.to(room).emit("sendMessage", { ...chat });
-  //       socket.emit("nowMessageId", { id: chat.id, chatLength });
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   });
+    const data = await chattingRepository.getChatsByUserId(userId);
 
-  //   // 이미지 보내기
-  //   socket.on("sendImage", async ({ room, name, uploadImage, chatLength }) => {
-  //     try {
-  //       const nowProject = await chatroom.findOne({
-  //         where: {
-  //           id: 1,
-  //         },
-  //       });
-  //       const nowUser = await user.findOne({
-  //         where: {
-  //           id: 1,
-  //         },
-  //       });
-  //       let chat = await chatting.create({
-  //         uploadImage,
-  //         text: "",
-  //         writer: nowUser,
-  //         project: nowProject,
-  //         room,
-  //       });
-  //       await chat.save();
-  //       socket.to(room).emit("sendImage", { ...chat });
-  //       socket.emit("nowMessageId", { id: chat.id, chatLength });
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   });
+    res.status(200).json(data);
+  };
 
-  //   // 메시지 조회
-  //   socket.on("getAllMessages", async ({ room, order }) => {
-  //     const nowProject = await chatroom.findOne({
-  //       where: {
-  //         id: 1,
-  //       },
-  //     });
-  //     const chats = await chatting.findAll({});
-  //     const COUNT_SCROLL = 30;
-  //     let isEnd = false;
-  //     let total = chats.length;
-  //     let start_chat = total - (order + 1) * COUNT_SCROLL;
-  //     if (start_chat < 0) {
-  //       // 마지막 chat인지 확인
-  //       start_chat = 0;
-  //       isEnd = true;
-  //     }
-  //     let end_chat = total - order * COUNT_SCROLL;
-  //     let sliceChats = chats.slice(start_chat, end_chat);
-  //     socket.emit("getAllMessages", { chats: sliceChats, isEnd });
-  //   });
+  getChat = async (req: Request, res: Response) => {
+    const chattingRepository = this.container.get<ChattingData>(
+      TYPES.chattingDB,
+    );
+    const id: number = Number(req.params.id);
 
-  //   // 방 퇴장
-  //   socket.on("leaveRoom", (room) => {
-  //     socket.leave(room);
-  //   });
-  // };
+    const chat = await chattingRepository.getChatById(id);
+    if (chat) {
+      res.status(200).json(chat);
+    } else {
+      res
+        .status(404)
+        .json({ message: `id: ${id} 는 존재하지 않는 메시지 입니다.` });
+    }
+  };
 
-  // createRoom = async (req: Request, res: Response) => {};
+  updateChat = async (req: Request, res: Response, next: NextFunction) => {
+    const chattingRepository = this.container.get<ChattingData>(
+      TYPES.chattingDB,
+    );
+    const userId: number = Number(req.cookies.id);
+    const id: number = Number(req.params.id);
+    const contents = req.body.contents;
 
-  // deleteRoom = async (req: Request, res: Response) => {};
+    const chat = await chattingRepository.getChatById(id);
+    if (!chat) {
+      return res
+        .status(404)
+        .json({ message: `id: ${id} 는 존재하지 않는 메시지 입니다.` });
+    }
+    if (chat.userId !== userId) {
+      return res.sendStatus(403);
+    }
+    const updated = await chattingRepository.updateChatById(id, contents);
+    return res.status(200).json(updated);
+  };
 
-  // getRoom = async (req: Request, res: Response) => {};
+  deleteChat = async (req: Request, res: Response, next: NextFunction) => {
+    const chattingRepository = this.container.get<ChattingData>(
+      TYPES.chattingDB,
+    );
+    const userId: number = Number(req.cookies.id);
+    const id: number = Number(req.params.id);
+
+    const chat = await chattingRepository.getChatById(id);
+    if (!chat) {
+      return res.status(404).json({ message: `Tweet not found: ${id}` });
+    }
+    if (chat.userId !== userId) {
+      return res.sendStatus(403);
+    }
+    await chattingRepository.deleteChatById(id);
+    return res.sendStatus(204);
+  };
 }
+
+//   // 방 입장
+//   socket.on("joinRoom", (room) => {
+//     socket.join(room);
+//   });
+
+//   //채팅 메시지
+//   socket.on("sendMessage", async ({ room, name, message, chatLength }) => {
+//     try {
+//       const nowProject = await chatroom.findOne({
+//         where: {},
+//       });
+//       const nowUser = await user.findOne({
+//         where: {},
+//       });
+//       let chat = await chatting.create({
+//         text: message,
+//         uploadImage: "",
+//         writer: nowUser,
+//         project: nowProject,
+//         room,
+//       });
+//       await chat.save();
+//       socket.to(room).emit("sendMessage", { ...chat });
+//       socket.emit("nowMessageId", { id: chat.id, chatLength });
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   });
+
+//   // 이미지 보내기
+//   socket.on("sendImage", async ({ room, name, uploadImage, chatLength }) => {
+//     try {
+//       const nowProject = await chatroom.findOne({
+//         where: {
+//           id: 1,
+//         },
+//       });
+//       const nowUser = await user.findOne({
+//         where: {
+//           id: 1,
+//         },
+//       });
+//       let chat = await chatting.create({
+//         uploadImage,
+//         text: "",
+//         writer: nowUser,
+//         project: nowProject,
+//         room,
+//       });
+//       await chat.save();
+//       socket.to(room).emit("sendImage", { ...chat });
+//       socket.emit("nowMessageId", { id: chat.id, chatLength });
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   });
+
+//   // 메시지 조회
+//   socket.on("getAllMessages", async ({ room, order }) => {
+//     const nowProject = await chatroom.findOne({
+//       where: {
+//         id: 1,
+//       },
+//     });
+//     const chats = await chatting.findAll({});
+//     const COUNT_SCROLL = 30;
+//     let isEnd = false;
+//     let total = chats.length;
+//     let start_chat = total - (order + 1) * COUNT_SCROLL;
+//     if (start_chat < 0) {
+//       // 마지막 chat인지 확인
+//       start_chat = 0;
+//       isEnd = true;
+//     }
+//     let end_chat = total - order * COUNT_SCROLL;
+//     let sliceChats = chats.slice(start_chat, end_chat);
+//     socket.emit("getAllMessages", { chats: sliceChats, isEnd });
+//   });
+
+//   // 방 퇴장
+//   socket.on("leaveRoom", (room) => {
+//     socket.leave(room);
+//   });
+// };
+
+// createRoom = async (req: Request, res: Response) => {};
+
+// deleteRoom = async (req: Request, res: Response) => {};
+
+// getRoom = async (req: Request, res: Response) => {};
